0f3a0c6ff8f592e61f919640ced2e5a0e5ea335b
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2025-03-22 09:20:26 +0000

    Add working networking example from aeronet

---
+++ minigolf_server/src/main.rs
@@ -0,0 +1,12 @@
+#[cfg(target_family = "wasm")]
+fn main() {
+    panic!("this example is not available on WASM");
+}
+
+#[cfg(not(target_family = "wasm"))]
+mod server;
+
+#[cfg(not(target_family = "wasm"))]
+fn main() {
+    server::main();
+}
